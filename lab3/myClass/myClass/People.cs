e22244a88eae17cbdd6196ddda69650de07f570a
romio-krvtsky <79285410+romio-krvtsky@users.noreply.github.com>
2021-04-02 15:11:12 +0300

    Add files via upload

---
+++ lab3/myClass/myClass/People.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace myClass
+{
+       public class People
+    {
+        List<Human> people;
+        public People()
+        {
+            people = new List<Human>();
+            amount = 0;
+        }
+
+        
+        public Human this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= amount)
+                {
+                    throw new Exception("There is no such index");
+                }
+                return people[index];
+            }
+            set
+            {
+                people.Insert(index, value);
+                ++amount;
+            }
+        }
+
+        private int amount;
+    }
+}
