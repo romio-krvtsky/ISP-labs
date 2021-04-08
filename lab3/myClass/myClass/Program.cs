4cf5604bccd5193d098ce7d458579aadc18ed051
romio-krvtsky <79285410+romio-krvtsky@users.noreply.github.com>
2021-04-08 13:53:35 +0300

    lr

---
+++ lab3/myClass/myClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myClass
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentOfSpeciality Vasya = new StudentOfSpeciality();
+            Vasya.Race = Human.Races.Caucasian;
+            Console.WriteLine(Vasya.Race);
+            Vasya.AcademicPerformance = StudentOfSpeciality.AcademicPermonances.average;
+            Console.WriteLine(Vasya.AcademicPerformance);
+            Vasya.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Vasya.Age);
+
+            People men = new People();
+            men[0] = Vasya;
+            Console.WriteLine("Race: {0}", men[0].Race);
+        }
+    }
+}
---
+++ lab3/myClass/myClass/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace myClass
+{
+    class Student : Human
+    {
+        public string University { get; set; }
+        public int YearOfEntering { get; set; }
+        public string Faculty { get; set; }
+    }
+
+    class StudentOfSpeciality : Student
+    {
+        public enum AcademicPermonances
+        {
+            awful,
+            bad,
+            average,
+            good,
+            excellent
+        }
+        public string Speciality { get; set; }
+        public int Group { get; set; }
+        public AcademicPermonances AcademicPerformance { get; set; }
+        public string Subject { get; set; }
+    }
+
+}
