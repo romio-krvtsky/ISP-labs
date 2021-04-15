b740cf80e0f1230dbfcab6e095b2d0e48732eb0c
romio-krvtsky <79285410+romio-krvtsky@users.noreply.github.com>
2021-04-16 01:34:54 +0300

    fix

--- lab3/myClass/myClass/Program.cs
+++ lab3/myClass/myClass/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 
 namespace myClass
@@ -22,28 +21,3 @@ namespace myClass
         }
     }
 }
-=======
-﻿using System;
-
-namespace myClass
-{
-    
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            StudentOfSpeciality Vasya = new StudentOfSpeciality();
-            Vasya.Race = Human.Races.Caucasian;
-            Console.WriteLine(Vasya.Race);
-            Vasya.AcademicPerformance = StudentOfSpeciality.AcademicPermonances.average;
-            Console.WriteLine(Vasya.AcademicPerformance);
-            Vasya.Age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Vasya.Age);
-
-            People men = new People();
-            men[0] = Vasya;
-            Console.WriteLine("Race: {0}", men[0].Race);
-        }
-    }
-}
->>>>>>> e22244a88eae17cbdd6196ddda69650de07f570a
