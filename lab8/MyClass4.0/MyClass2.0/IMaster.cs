32fc76f55e982c5ab34931b1fced326f77f8596b
romio-krvtsky <79285410+romio-krvtsky@users.noreply.github.com>
2021-05-10 18:13:53 +0300

    update

---
+++ lab8/MyClass4.0/MyClass2.0/IMaster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyClass2._0
+{
+    interface IMaster
+    {
+        enum Degrees { }
+        int Course { get; set; }
+        void UpDegree();
+    }
+}
